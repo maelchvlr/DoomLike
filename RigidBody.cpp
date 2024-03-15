4ad80f92c8591b4ab098bad83b80cacbca8ec91c
Enzo Martinez <killerbackofheart@gmail.com>
2024-03-15 13:12:12 +0100

    Maintenant la physique n'intervient plus lors du calcul de la chute de l'objet

--- RigidBody.cpp
+++ RigidBody.cpp
@@ -13,8 +13,8 @@ RigidBody::~RigidBody() {}
 //Formule de la position : x(t) = x0 + v0*t + 1/2*a*t^2
 void RigidBody::update(float dt) {
 	if(!movable || !simulate) return;
-	acceleration = glm::vec3(0.0f * mass, -9.81f * mass, 0.0f * mass);
-	position = position + velocity * dt + 0.5f * acceleration * (dt * dt);
+	acceleration = glm::vec3(0.0f, -9.81f, 0.0f);
+	position = position + velocity * dt;
 	velocity += acceleration * dt;
 }
 
