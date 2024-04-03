21051bbb9d0880a797dfbf8484cc0b6ef0bd7c8e
Enzo <enzo111001@live.fr>
2024-04-03 19:04:37 +0200

    Mise en place d'un systeme de lecture de coordonnees exterieur au fichier .cpp afin de permettre une création de terrain via fichier .txt dans le dossier ressource

--- Terrain.cpp
+++ Terrain.cpp
@@ -3,18 +3,41 @@
 #include <vector>
 #include <gtc/matrix_transform.hpp>
 #include <gtc/type_ptr.hpp>
+#include <fstream>
+#include <iostream>
 
 Terrain::Terrain( glm::vec3 topLeft, glm::vec3 _size, bool _Textured, Texture* _Texture, float mass, float restitution, bool movable)
     : Models(mass, restitution, movable, _Textured, _Texture, topLeft, _size), size(_size)
 {
+    std::ifstream fichier("./ressources/map/tile01.txt");
+    const int taille = 20; //Taille de lecture défini à 20 car on récupère un Terrain, A CHANGER POUR LES AUTRES OBJETS
+    float vertices[taille];
+
+    if (fichier) {
+        std::cout << "Ouverture du fichier tile01.txt reussie !" << std::endl;
+
+        for (int i = 0; i < taille; i++) {  
+            float valeur;
+            fichier >> valeur; // On lit un chiffre dans le fichier (déplace le curseur et s'arrête à l'espace)
+
+            vertices[i] = valeur;
+
+            std::cout << valeur << std::endl;
+        }
+        fichier.close();
+    }
+	else {
+		std::cout << "Erreur lors de l'ouverture du fichier tile01.txt !" << std::endl;
+    }
+
     // Define vertices for a flat grid
-    float vertices[] = {
-        // Positions        // Texture Coords 
-        0.0f, 0.0f, 0.0f,    0.0f, 0.0f,
-        1.0f, 0.0f, 0.0f,    1.0f, 0.0f,
-        1.0f, 0.0f, 1.0f,    1.0f, 1.0f,
-        0.0f, 0.0f, 1.0f,    0.0f, 1.0f,
-    };
+    //float vertices[] = {
+    //    // Positions        // Texture Coords
+    //    0.0f, 0.0f, 0.0f,    0.0f, 0.0f,
+    //    1.0f, 0.0f, 0.0f,    1.0f, 0.0f,
+    //    1.0f, 0.0f, 1.0f,    1.0f, 1.0f,
+    //    0.0f, 0.0f, 1.0f,    0.0f, 1.0f,
+    //};
     unsigned int indices[] = {
         0, 1, 2, // First Triangle
         2, 3, 0  // Second Triangle
